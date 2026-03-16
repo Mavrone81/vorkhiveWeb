bd505e39b8d5ab857a5a91d9a35e37e2b0549375
levijanclavesillas <levijanclavesillas@gmail.com>
2026-03-16 20:48:10 +0800

    updated server.js tyo comply with express v.4

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
 });
 
 // Handle SPA routing: ALWAYS serve index.html for non-API routes
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
